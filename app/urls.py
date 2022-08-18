f4219a4d134a5a50d5107958d0b63f7e5add25dc
demirug <pm.antondolia@gmail.com>
2022-08-18 23:36:28 +0300

    Registered urls

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(('apps.products.urls', 'products')))
+    path('', include(('apps.orders.urls', 'orders'))),
+    path('', include(('apps.products.urls', 'products'))),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
