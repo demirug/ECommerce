8e4798c256d017d37c56f4fce5ef2178c385450f
demirug <pm.antondolia@gmail.com>
2022-08-18 23:36:57 +0300

    Provides "add to cart" button

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,6 +1,9 @@
+from django.shortcuts import redirect
 from django.urls import reverse
+from django.views.generic.edit import ProcessFormView
 from django_jinja.views.generic import ListView, DetailView
 
+from apps.orders.cart import Cart
 from apps.products.models import Product
 from shared.mixins.breadcrumbs import BreadCrumbsMixin
 from shared.mixins.paginator import RemovePageMixin
@@ -18,6 +21,11 @@ class ProductListView(RemovePageMixin, ListView):
 class ProductDetailView(BreadCrumbsMixin, DetailView):
     model = Product
     template_name = "products/detail.jinja"
+
+    def post(self, request, *args, **kwargs):
+        cart: Cart = Cart(request)
+        cart.add(self.get_object(), 1)
+        return redirect("orders:cart")
 
     def get_queryset(self):
         return Product.objects.filter(enable=True)
