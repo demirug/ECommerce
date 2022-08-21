da3bd8bdbeebbc18378b33a28487d217ee25580d
demirug <pm.antondolia@gmail.com>
2022-08-22 00:17:41 +0300

    Fix incorrect name

--- apps/orders/constants.py
+++ apps/orders/constants.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class OrderStatus(TextChoices):
-    NEW = "NEW", _("NEWm")
+    NEW = "NEW", _("NEW")
     PAYED = "PAYED", _("PAYED")
     DONE = "DONE", _("Done"),
     CANCELED = "CANCELED", _("CANCELED")
