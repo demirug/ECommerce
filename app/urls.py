7f46eccef530eb1db1d547c0de76aba6f0fb8885
demirug <pm.antondolia@gmail.com>
2022-08-22 22:42:18 +0300

    Register helper url

--- app/urls.py
+++ app/urls.py
@@ -3,12 +3,17 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from apps.helper.views import FeedbackFAQView
 from apps.textpage.views import TextPageDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+
+
     path("page/<slug:slug>/", TextPageDetailView.as_view(), name="textpage"),
+    path("helper/", FeedbackFAQView.as_view(), name="helper"),
+
     path('', include(('apps.orders.urls', 'orders'))),
     path('', include(('apps.products.urls', 'products'))),
 ]
