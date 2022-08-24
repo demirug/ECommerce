b2a673562e2cd614d2fda0dcd052e8c005a255e0
demirug <pm.antondolia@gmail.com>
2022-08-24 19:21:45 +0300

    Register OrderSettings

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from solo.admin import SingletonModelAdmin
 
-from apps.orders.models import Order
+from apps.orders.models import Order, OrderSettings
 
 admin.site.register(Order)
+admin.site.register(OrderSettings, SingletonModelAdmin)
